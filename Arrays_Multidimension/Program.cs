7842c9f1ebcd754b1b55456ce8f6cba0de581f5f
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-04-15 17:25:34 -0300

    Alura course - Collection 2 - Jagged Arrays

--- Arrays_Multidimension/Program.cs
+++ Arrays_Multidimension/Program.cs
@@ -10,7 +10,32 @@ namespace Arrays_Multidimension
     {
         static void Main(string[] args)
         {
+            string[,] result = new string[3, 3]
+            {
+                {"Alemanha", "Espanha", "Itália" },
+                {"Argentina", "Holanda", "França" },
+                {"Holanda", "Alemanha", "Alemanha"}
+            };
 
+            
+            for (int copa = 0; copa < 3; copa++)
+            {
+                int ano = 2014 - (copa * 4);
+                Console.Write(ano.ToString().PadRight(14));
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i <= result.GetUpperBound(0); i++)
+            {
+                
+                for (int j = 0; j <= result.GetUpperBound(1); j++)
+                {
+                    Console.Write($"{result[i, j].PadRight(14)}");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.ReadKey();
         }
     }
 }
---
+++ Teste/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] tabuleiro = new char[3, 3];
+            tabuleiro[0, 0] = 'X';
+            tabuleiro[1, 0] = '.';
+            tabuleiro[2, 0] = 'O';
+            tabuleiro[0, 1] = 'X';
+            tabuleiro[1, 1] = 'X';
+            tabuleiro[2, 1] = 'O';
+            tabuleiro[0, 2] = '.';
+            tabuleiro[1, 2] = 'O';
+            tabuleiro[2, 2] = 'X';
+
+            for (var y = 0; y < 3; y++)
+            {
+                for (var x = 0; x < 3; x++)
+                {
+                    Console.Write(tabuleiro[x, y]);
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
